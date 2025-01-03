543cdfe027b2c627911288ecd304b3335a18ae9b
odinsubject09 <your-email@example.com>
2025-01-03 23:36:00 +0530

    google OAuth added

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,7 +4,7 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 import { toast } from 'react-toastify'
-
+import OAuth from '../components/OAuth'
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
@@ -80,6 +80,7 @@ function SignIn() {
             </button>
           </div>
         </form>
+        <OAuth />
         <Link to='/sign-up' className='registerLink'>
           Sign Up Instead
         </Link>
