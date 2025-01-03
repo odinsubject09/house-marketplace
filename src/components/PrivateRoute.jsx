f69b517e9c38cee810acee0a99b93fe32f6bd545
odinsubject09 <your-email@example.com>
2025-01-03 18:51:25 +0530

    privateRoute component for SignIn mechanism and useAuthStatus hook

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,14 @@
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
+
 const PrivateRoute = () => {
-  return (
-    <div>PrivateRoute</div>
-  )
+    const { loggedIn, checkingStatus } = useAuthStatus()
+
+    if (checkingStatus) {
+        <Spinner />
+    }
+    return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
 }
 
 export default PrivateRoute
