942b3cb698d1e1a0e3fba4622c5668631b98e9e8
odinsubject09 <your-email@example.com>
2025-02-11 22:13:50 +0530

    changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect ,useCallback} from 'react'
 import { Link } from 'react-router-dom'
 import { getAuth, updateProfile, updateEmail } from 'firebase/auth'
 import {
